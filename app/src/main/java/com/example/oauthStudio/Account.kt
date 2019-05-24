9e697fdc4d1c76df5114a6b1cc4ca5606873bebc
Dmitrii Stepcenco <dmitriistepcenco@MacBook-Pro-Dmitrii.local>
2019-05-24 08:53:34 +0300

    Added Account model class

---
+++ app/src/main/java/com/example/oauthStudio/Account.kt
@@ -0,0 +1,8 @@
+package com.example.oauthStudio
+
+class Account {
+    var userName: String? = ""
+    var profilePictureLink: String? = ""
+    var dateOfBirth: String? = ""
+    var email: String? = ""
+}
